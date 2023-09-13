dbd5c77de206406b9d8e28d25373aa70a19087bf
Julyemerson Leonizio <jb.leonizio@gmail.com>
2023-09-13 12:57:14 -0300

    add form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
+  constructor() {
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 
+  text: string;
+  day: string;
+  reminder: boolean = false;
 }
